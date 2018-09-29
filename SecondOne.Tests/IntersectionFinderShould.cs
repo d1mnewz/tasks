544ff3d832d4e5cc1dfb562c2e9c1ba431ddeb79
Dmytro Zhluktenko <d1mnewz@gmail.com>
2018-09-30 01:51:20 +0300

    add test for second task

---
+++ SecondOne.Tests/IntersectionFinderShould.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SecondOne.Tests
+{
+    // find intersection between 3 sorted arrays
+    // [2,6,7,8]
+    // [3,6,8,10]
+    // [4,5,6,8]
+
+    [TestFixture]
+    public class IntersectionFinderShould
+    {
+        [TestCase(new[] { 2, 6, 7, 8 }, new[] { 3, 6, 8, 10 }, new[] { 4, 5, 6, 8 }, new[] { 6, 8 })]
+        public void FindIntersection(int[] arr1, int[] arr2, int[] arr3, int[] intersection)
+        {
+            var sut = new IntersectionFinder();
+
+            var result = sut.FindIntersection(arr1, arr2, arr3);
+            result.Should().NotBeNull();
+            result.Count().Should().Be(intersection.Length);
+            result.Should().AllBeEquivalentTo(intersection);
+        }
+    }
+
+    public class IntersectionFinder
+    {
+        public IEnumerable<int> FindIntersection(int[] arr1, int[] arr2, int[] arr3)
+        {
+            return null;
+        }
+    }
+}
